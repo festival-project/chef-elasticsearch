4073d8bcce8a08d99a283aee832ad66a81583137
Chef Test User@JOSE <ubuntu@chef-test>
2016-10-20 12:05:47 +0000

    add first recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+apt_update 'Update the apt cache' do
+  action :update
+end
+
+apt_package 'openjdk-8-jdk' do
+  action :install
+end
+
+
